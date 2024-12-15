df3bc1dfbe8da10b6058848620dca7cd51f2b276
yibeltal marie <yibeltalmarie1228@gmail.com>
2024-12-15 23:40:08 +0300

    drawing a diamond shape

---
+++ making_patterns/diamond_pattern.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+for (int i = 1; i <= 6; i++) {
+    for (int j = 1; j <= 6 + i - 1; j++) {
+        if (j <= 6 - i) {
+            cout << " ";
+        }else if (j == 6 - i + 1 || j == 6 + i - 1 ){
+            cout << "*"; } else {
+                cout << " ";
+            }
+    } cout<<endl;
+    }
+for (int i = 6; i>=1; i--) {
+    for (int j = 1; j <= 6 + i - 1; j++) {
+        if (j <= 6 - i) {
+            cout << " ";
+        }else if (j == 6 - i + 1 || j == 6 + i - 1 ){
+            cout << "*"; } else {
+                cout << " ";
+            }
+    } cout<<endl;
+    }
+return 0;
+}
